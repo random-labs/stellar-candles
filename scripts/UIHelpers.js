42dc6e1ee496d864ddb7864daf78e1123c7f290e
rarach <martin.mihalovic@post.com>
2018-08-31 23:51:20 -0400

    Renamed custom HTML tag include to sc-include to conform to convention

--- scripts/UIHelpers.js
+++ scripts/UIHelpers.js
@@ -1,9 +1,9 @@
 $(function(){
     /**
-     * Static components loading engine. Replaces any <include src='component.html'> element with respective
+     * Static components loading engine. Replaces any <sc-include src='component.html'> element with respective
      * markup defined in the file from "src" attribute. Dynamic templates are currently not supported.
      */
-    $("include").each(function() {
+    $("sc-include").each(function() {
         var sourceFile = $(this).attr("src");
         if (sourceFile) {
             $(this).load(sourceFile);
